0616b30e3b00a8efef5dcc5dc1db2ccfd65aafcb
vasamsettirakesh143-collab <vasamsettirakesh143@gmail.com>
2026-08-14 11:14:21 +0530

    Update VamshiForegroundService.java

--- native/VamshiForegroundService.java
+++ native/VamshiForegroundService.java
@@ -27,12 +27,14 @@ public class VamshiForegroundService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Notification notification = buildNotification();
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             startForeground(
                 NOTIFICATION_ID,
                 notification,
-                ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE
             );
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(NOTIFICATION_ID, notification);
         } else {
             startForeground(NOTIFICATION_ID, notification);
         }
