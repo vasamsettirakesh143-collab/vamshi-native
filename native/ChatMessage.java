83e5264b335b8a0c5dea37767a7ea98d4faa9ede
vasamsettirakesh143-collab <vasamsettirakesh143@gmail.com>
2026-09-07 22:10:48 +0530

    Create ChatMessage.java

---
+++ native/ChatMessage.java
@@ -0,0 +1,10 @@
+public class ChatMessage {
+
+    public String text;
+    public boolean isUser;
+
+    public ChatMessage(String text, boolean isUser) {
+        this.text = text;
+        this.isUser = isUser;
+    }
+}
