75e6642c56ba3ba9dfc8bd958e4754126b87e0d9
vasamsettirakesh143-collab <vasamsettirakesh143@gmail.com>
2026-09-07 22:12:14 +0530

    Create ChatAdapter.java

---
+++ native/ChatAdapter.java
@@ -0,0 +1,59 @@
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+
+    private final ArrayList<ChatMessage> messages;
+
+    public ChatAdapter(ArrayList<ChatMessage> messages) {
+        this.messages = messages;
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.chat_item, parent, false);
+
+        return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+
+        ChatMessage message = messages.get(position);
+
+        holder.messageText.setText(message.text);
+
+        if (message.isUser) {
+            holder.messageText.setBackgroundResource(
+                    R.drawable.bubble_user);
+        } else {
+            holder.messageText.setBackgroundResource(
+                    R.drawable.bubble_bot);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    static class ChatViewHolder extends RecyclerView.ViewHolder {
+
+        TextView messageText;
+
+        ChatViewHolder(View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.messageText);
+        }
+    }
+}
